97cfc0044e71370ff20d0b4d22aa1fba3176c25a
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2026-04-03 23:55:35 +0530

    Update shell.py

--- shell.py
+++ shell.py
@@ -8,7 +8,21 @@ dcslkj
 cnjkdscks
 dsjkcs
 dcnjs
+cvsdjndcslcnls
+dcslkj
+cnjkdscks
+dsjkcs
+dcnjs
 cvsdjn
+vscddls
+ssdmaa
+sjldskndcsdskjfnjkadsk
+dsjnsla
+casln
+def
+function a
+
+
 vscddls
 ssdmaa
 sjldskndcsdskjfnjkadsk
